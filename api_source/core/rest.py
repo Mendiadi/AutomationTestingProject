b66c1a444400eed814662b918194e11597fed06a
mendiadi <adim333@gmail.com>
2022-09-08 12:39:00 +0300

    rest.py hint

--- api_source/core/rest.py
+++ api_source/core/rest.py
@@ -1,3 +1,64 @@
+"""
+    Written by adi mendel 6.8.2022
+    my implementation for rest api using decorator factory.
+    you can send requests from your api class using my decorators as
+    @rest.get() - for get request
+    @rest.post() for post request
+    @rest.delete() for delete request
+    @rest.put() for put request
+    to use it properly you need to attention for some details!
+    (1) you need in your api class method calls to give only one parameter as name 'response' (self also include)
+    example (1): in your api class :
+                SomeApi():
+                    def __init__(self):
+                        pass
+
+                    @rest.post()
+                    def post_user(self,response):
+                        return response # to return the result or return any manipulation
+    (2) you can pass the url or just part of it using url param config like that:
+    example (2):
+            SomeApi():
+                def __init__(self):
+                    pass
+
+                @rest.post(url="myUrl") # as you can see you can config url
+                def post_user(self,response):
+                    return response # to return the result or return any manipulation
+    (3) also you can config the params to the url mostly like this:
+        SomeApi():
+                def __init__(self):
+                    pass
+
+                @rest.post(url="myUrl",param="name")
+                def post_user_name(self,response):
+                    return response # to return the result or return any manipulation
+
+                @rest.post(url="myUrl",param="id")
+                def post_user_id(self,response):
+                    return response # to return the result or return any manipulation
+
+        ** in your test file you use it like:
+            def test_user():
+                res = post_user_name(name="sample")
+                res = post_user_id(id="sample")
+                # as you can see you can call these api methods and -
+                use the parameter config to send most you need to.
+
+    (4) you can always send data with your request
+    example (4):
+        in your test file -
+            def test_user():
+                res = post_user(data={name:name,id:id})
+
+                # as you can see you can always send or ignore and then data will be None
+
+    after im showed you how to use it and example also
+    you can simply try and use it its very cool decorators.
+
+    more will come out soon, adi.
+"""
+
 import requests
 
 GET = "GET"
